baa6b904749c2eae80465f09a7d46fbf7965580e
Pradeep Rai <42033426+pradeeprai-82@users.noreply.github.com>
2022-05-17 21:26:24 +0530

    upload

---
+++ main.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from tkinter import messagebox
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password =password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="oops", message="Please make sure all details are entered")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the detail entered: \n{email}\n"
+                                                f"\npassword: {password} \nIs it OK to save?")
+        if is_ok:
+
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0,END)
+                website_entry.delete(0,END)
+
+
+window= Tk()
+window.title("Password Manager")
+window.config(padx=40, pady=20)
+
+canvas = Canvas(height=280, width=280)
+
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100,100, image=logo_image)
+
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email")
+email_label.grid(row=2, column=0)
+
+password_label =Label(text="Password")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "pc@email.com")
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", command=save)
+add_button.grid(row=4, column=1)
+
+window.mainloop()
